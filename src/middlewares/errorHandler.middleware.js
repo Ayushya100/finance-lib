0a89eab4a25a335493c7419fc15dd3663308abfd
Ayushya <jaiswalayushya@gmail.com>
2025-05-22 21:48:37 +0530

    Fixed issue with error handler function for returning the error to the user

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -4,13 +4,26 @@ import { buildApiError } from '../utils/index.js';
 
 const errorHandler = (err, req, res, next) => {
   const apiError = buildApiError(err);
+
+  let errors = [];
+  if (Array.isArray(apiError.errors)) {
+    errors = apiError.errors.map(e => e?.message || e);
+  } else if (apiError.errors instanceof Error) {
+    errors = [apiError.errors.message];
+  } else if (typeof apiError.errors === 'string') {
+    errors = [apiError.errors];
+  } else if (apiError.errors) {
+    errors = [apiError.errors];
+  }
+
   res.status(err.status).json({
     statusCode: apiError.statusCode,
     message: apiError.message,
     devMessage: apiError.devMessage,
     type: apiError.type,
-    errors: apiError.errors,
+    errors: errors,
     data: apiError.data,
+    stack: apiError.stack,
     success: apiError.success,
   });
 };
